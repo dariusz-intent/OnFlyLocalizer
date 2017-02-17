8bf8fca48705340266dbc516476889d8bdf5202f
iljakosynkin <ilja.kosynkin@gmail.com>
2017-02-17 16:25:34 +0100

    Initial Commit

---
+++ OnFlyLocalizer/OnFlyLocalizer.h
@@ -0,0 +1,19 @@
+//
+//  OnFlyLocalizer.h
+//  OnFlyLocalizer
+//
+//  Created by Ilja Kosynkin on 2/17/17.
+//  Copyright © 2017 Ilja Kosynkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OnFlyLocalizer.
+FOUNDATION_EXPORT double OnFlyLocalizerVersionNumber;
+
+//! Project version string for OnFlyLocalizer.
+FOUNDATION_EXPORT const unsigned char OnFlyLocalizerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OnFlyLocalizer/PublicHeader.h>
+
+
